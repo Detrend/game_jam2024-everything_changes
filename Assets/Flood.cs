5e05fa705470d06e8fa44fd63031d097cf9f6dc6
Detrend <0.optima.0@gmail.com>
2024-11-30 16:44:49 +0100

    floood++

--- Assets/Flood.cs
+++ Assets/Flood.cs
@@ -11,10 +11,38 @@ public class Flood : MonoBehaviour
   public float Duration = 15.0f;
 
   float m_Duration = 0.0f;
+  GameObject m_Water = null;
 
   void Start()
   {
     m_Duration = Duration;
+    m_Water = transform.Find("Water")?.gameObject;
+
+    StartCoroutine(StartingCoroutine());
+  }
+
+  IEnumerator StartingCoroutine()
+  {
+    for (int i = 0; i < 100; ++i)
+    {
+      m_Water.transform.position += new Vector3(0.0f, 1.0f, 0.0f) * 0.01f;
+      yield return new WaitForSeconds(0.1f);
+    }
+
+    yield return null;
+  }
+
+  IEnumerator EndCoroutine()
+  {
+    for (int i = 0; i < 100; ++i)
+    {
+      m_Water.transform.position += new Vector3(0.0f, -1.0f, 0.0f) * 0.01f;
+      yield return new WaitForSeconds(0.1f);
+    }
+
+    GameObject.Destroy(gameObject);
+
+    yield return null;
   }
 
   void Update()
@@ -26,9 +54,9 @@ public class Flood : MonoBehaviour
       // deal damage to blocks
       for (int h = 0; h < Height; ++h)
       {
-        for (int i = 0; i < Game.I.HouseGrid.BBox.Size.X; ++i)
+        for (int i = 0; i < Game.I.gameRegion.Size.X; ++i)
         {
-          var block = Game.I.HouseGrid[new IVector2(i, h) + Game.I.HouseGrid.BBox.from];
+          var block = Game.I.HouseGrid[new IVector2(i, h) + Game.I.gameRegion.from];
           if (block != null)
           {
             // damage the block
@@ -38,7 +66,7 @@ public class Flood : MonoBehaviour
     }
     else
     {
-      GameObject.Destroy(gameObject);
+      StartCoroutine(EndCoroutine());
     }
   }
 }
